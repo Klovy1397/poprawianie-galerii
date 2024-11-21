834e6df6b45694cc9bdef042aa71deabc0657fb7
Klovy <108542503+Klovy1397@users.noreply.github.com>
2024-11-21 11:25:45 +0100

    Update cal.php
    Aktualizacja pliku.

--- cal.php
+++ cal.php
@@ -19,24 +19,25 @@
 
     $highlightedDate = "20-11-2024"; 
     $currentWeekStart = date('d-m-Y', strtotime('monday this week'));
-    $currentWeekEnd = date('d-m-Y', strtotime('sunday this week')); 
+    $currentWeekEnd = date('d-m-Y', strtotime('sunday this week'));
 
+    // Obsługa zmiennych GET
+    $startOffset = isset($_GET['start_offset']) ? intval($_GET['start_offset']) : -dniDoPoprzPon();
+    $endOffset = isset($_GET['end_offset']) ? intval($_GET['end_offset']) : 30 - dniDoPoprzPon();
 
-    for ($i = -dniDoPoprzPon(); $i < 30 - dniDoPoprzPon(); $i++) {
+    // Wyświetlanie kalendarza w zadanym zakresie
+    for ($i = $startOffset; $i < $endOffset; $i++) {
         $currentDate = liczbaNaDate($i);
         $isSunday = date('N', strtotime("{$i} days")) == 7;
         $dodatkowe_klasy = $isSunday ? ' niedziela' : '';
-
 
         if ($currentDate >= $currentWeekStart && $currentDate <= $currentWeekEnd) {
             $dodatkowe_klasy .= ' this_week';
         }
 
-
         if ($currentDate == date("d-m-Y")) {
             $dodatkowe_klasy .= ' dzis';
         }
-
 
         if ($currentDate == $highlightedDate) {
             $dodatkowe_klasy .= ' wyrozniony';
@@ -45,4 +46,12 @@
         echo '<div class="day-box' . $dodatkowe_klasy . '">' . $currentDate . '</div>';
     }
     ?>
+</div>
+
+<div class="controls">
+    <br>
+    <a href="?start_offset=<?php echo $startOffset - (4 * 7); ?>&end_offset=<?php echo $endOffset; ?>">Dodaj 4 wcześniejsze tygodnie</a>
+    <br>
+    <br>
+    <a href="?start_offset=<?php echo $startOffset; ?>&end_offset=<?php echo $endOffset + (8 * 7); ?>">Dodaj 8 następnych tygodni</a>
 </div>
